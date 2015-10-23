2303891b5109b4b95db6e1f21ec25fadc80c0b67
igor venegas <venegas.igor@gmail.com>
2015-10-23 20:13:06 -0300

    add validate post

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ActiveRecord::Base
+
+	  validates :title, presence: true
+  	  validates :description, presence: true
+  	  validates :price, presence: true
+  	  validates :stock, presence: true
+
 end
